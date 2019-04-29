b1e33c02032dd44ca92b7894c8fd7d84f742510c
TG7789RR <43755284+TG7789RR@users.noreply.github.com>
2019-04-29 16:10:01 +0100

    Create 190423_caraccident_model.R
    car accident practice model, in progress

---
+++ 190423_caraccident_model.R
@@ -0,0 +1,34 @@
+
+
+library(rpart)
+library(rpart.plot)
+library(caTools)
+library(dplyr)
+
+acc<-read.table("Z:/Thurstan/190423 car accidents/acc_Rin.csv"
+                ,comment.char = "", fill=TRUE,sep=",",header = T)
+
+
+
+
+#do a train test split 
+sample1 = sample.split(acc$NumberVech, SplitRatio = 0.7)
+training_set = subset(acc, sample1 == TRUE)
+test_set = subset(acc, sample1 == FALSE)
+
+nrow(training_set)
+nrow(test_set)
+
+
+still need to check variable balance between the two 
+
+#below needs fixing for train / test setup
+
+acc[0:3,]
+colnames(acc)
+nrow(acc)
+
+tree1 <- rpart(T_fatal_or_serious ~ NumberVech+NumberCasualties, data = acc)
+rpart.plot(tree1)
+
+T_fatal_or_serious
